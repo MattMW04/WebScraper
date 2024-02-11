f4148cc2230e6313e2b80bc4991e062211ad3d85
MattMW04 <6wilcm63@solent.ac.uk>
2024-02-11 00:57:40 +0000

    scraper gets content from divs

--- PremWebScraper.py
+++ PremWebScraper.py
@@ -6,9 +6,14 @@ response = requests.get(url)
 if response.status_code ==200:
     content = response.content
     print("Success getting content")
-    print(content)
     BFS = BeautifulSoup(content, 'html.parser')
+
+    elements = BFS.find_all('div', class_='top-stats__row-stat')
+    for element in elements:
+        print(element)
 else:
     print("Fail: Error getting content")
+
+
 
 
