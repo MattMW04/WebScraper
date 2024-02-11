0d4881d426ea997d1a3f2ba5065e95ad141d28d0
MattMW04 <6wilcm63@solent.ac.uk>
2024-02-11 01:13:49 +0000

    put into tkinter window

--- PremWebScraper.py
+++ PremWebScraper.py
@@ -1,5 +1,6 @@
 from bs4 import BeautifulSoup
 import requests
+import tkinter as tk
 url ='https://www.premierleague.com/stats'
 response = requests.get(url)
 
@@ -9,8 +10,15 @@ if response.status_code ==200:
     BFS = BeautifulSoup(content, 'html.parser')
 
     elements = BFS.find_all('div', class_='top-stats__row-stat')
+    window = tk.Tk()
     for element in elements:
+        text =tk.Text(window)
+        text.insert(tk.END, element)
+        text.pack()
         print(element)
+
+
+    window.mainloop()
 else:
     print("Fail: Error getting content")
 
